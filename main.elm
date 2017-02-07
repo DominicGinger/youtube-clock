afeca2ef2c175f0f3e52e19581a838fff4eda83a
Dominic Ginger <domginger@hotmail.co.uk>
2017-02-07 23:03:40 +0000

    Initial commit, working youtube clock without autoplay

---
+++ main.elm
@@ -0,0 +1,69 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Time exposing (Time, second)
+import Date exposing (..)
+
+
+youtubeUrl =
+    "https://www.youtube.com/embed?listType=search&list="
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( youtubeUrl, Cmd.none )
+
+
+type Msg
+    = Tick Time
+
+
+displayPartTime fn time =
+    toString (fn (fromTime time))
+
+
+displayAmPm time =
+    if (Date.minute (fromTime time)) > 12 then
+        "pm"
+    else
+        "am"
+
+
+displayTime time =
+    (displayPartTime Date.hour time)
+        ++ ":"
+        ++ (displayPartTime Date.minute time)
+        ++ displayAmPm time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick time ->
+            ( youtubeUrl ++ (displayTime time), Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every Time.minute Tick
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "ytPlayer" ]
+        [ iframe [ src model, style [ ( "width", "100%" ), ( "height", "100%" ) ] ] []
+        ]
